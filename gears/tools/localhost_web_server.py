03f657ec16de19c43790f359c9be08878e5d020e
gears.daemon <gears.daemon@fe895e04-df30-0410-9975-d76d301b4276>
2007-08-07 19:00:31 +0000

    [Author: cprince]
    Support mapping localhost root to another directory.
    
    R=aa
    CC=google-gears-eng@googlegroups.com
    DELTA=8  (5 added, 0 deleted, 3 changed)
    OCL=5175606
    SCL=5181496

--- gears/tools/localhost_web_server.py
+++ gears/tools/localhost_web_server.py
@@ -1,13 +1,18 @@
 import os
+import sys
 import BaseHTTPServer
 import SimpleHTTPServer
 
 def main():
+  # If present, argv[1] is the directory to use as the localhost root.
+  if len(sys.argv) > 1:
+    os.chdir(sys.argv[1])
+
   print '''
-Started HTTP server.  http://localhost/... maps to the current directory:
-  %s
+Started HTTP server.  http://localhost/... maps to:
+  %s%s...
 Press Ctrl-Break or Ctrl-C to exit.
-''' % os.getcwd()
+''' % (os.getcwd(), os.sep)
 
   server_class = BaseHTTPServer.HTTPServer
   handler_class = SimpleHTTPServer.SimpleHTTPRequestHandler
