043a710a64a5ea7530f4d060ce5f1a41cbd3cab0
gears.daemon <gears.daemon@fe895e04-df30-0410-9975-d76d301b4276>
2008-03-11 00:02:07 +0000

    [Author: michaeln]
    A stab at addressing a timing issue with our HttpRequest tests
    
    R=aa
    CC=google-gears-eng@googlegroups.com
    DELTA=7  (6 added, 0 deleted, 1 changed)
    OCL=6653959
    SCL=6656930

--- trunk/gears/test/testcases/cgi/send_response_of_size.py
+++ trunk/gears/test/testcases/cgi/send_response_of_size.py
@@ -1,11 +1,17 @@
 size = int(self.query['size'][0])
+slowly = self.query.has_key('slowly')
 
 self.send_response(200)
-self.send_header('Content-Type', 'text/plain')
+self.send_header('Content-Type', 'text/html')
 self.send_header('Content-Length', size)
 self.end_headers()
 
+if slowly:
+  time.sleep(0.5)
+  
 for i in xrange(0, size):
+  if slowly:
+    time.sleep(0.01);
   self.outgoing.append('a')
 else:
   self.outgoing.append (None)
