5db160a2c2865fefee7f5c08bd31d27cdf803bb6
gears.daemon <gears.daemon@fe895e04-df30-0410-9975-d76d301b4276>
2008-04-28 21:00:59 +0000

    [Author: ace]
    Add firefox3 testing to windows build runs.
    
    R=playmobil
    CC=gears-internal
    APPROVED=playmobil
    DELTA=173  (106 added, 34 deleted, 33 changed)
    OCL=7011399
    SCL=7044203

--- gears/test/runner/browser_launchers.py
+++ gears/test/runner/browser_launchers.py
@@ -1,13 +1,31 @@
 import os
 import subprocess
 import signal
+import time
 
 class BrowserLauncher:
   """ Handle launching and killing of a specific test browser. """
+  # Time given to allow firefox to update test profile and close.
+  FIREFOX_QUITTING_SLEEP_TIME = 15 #seconds
+
   def launch(self, url):
-    """ Do launch. 
+    """ Externally facing launch command.
+
+    The default launch is overloaded in most implementations.
+    
+    Args:
+      url: Url to launch
+    """
+    self._launchImp(url)
+
+
+  def _launchImp(self, url):
+    """ Launch browser to the given url and track the returned process.
     
     Handle chrome addresses by using the -chrome parameter.
+
+    Args:
+      url: Url to launch
     """
     command = [self.browser_path]
     command.extend(self.args)
@@ -16,8 +34,27 @@ class BrowserLauncher:
     command.append(url)
     print 'launching: %s' % str(command)
     self.process = subprocess.Popen(command)
-
   
+
+  def _startFirefox(self, url):
+    """ Prep Firefox by launching quit, then launch url.
+
+    Firefox restarts itself when it is first launched after installing
+    a new addon.  In order to track the process by pid, we launch
+    the browser to ensure addon installation is complete and call
+    the provided quit function, then launch the browser
+    a second time to the specified url.
+
+    Args:
+      url: Url to launch
+    """
+    quit_url = 'chrome://quit/content/quit.html'
+    # Launch quit then wait for Firefox to close completely.
+    self._launchImp(quit_url)
+    time.sleep(BrowserLauncher.FIREFOX_QUITTING_SLEEP_TIME)
+    self._launchImp(url)    
+
+
   def kill(self):
     """ Kill the browser process for clean-up. """
     print 'killing process: %s' % str(self.process.pid)
@@ -26,7 +63,8 @@ class BrowserLauncher:
 
 class Win32Launcher(BrowserLauncher):
   """ Launcher for Win32 platforms. """
-  def launch(self, url):
+
+  def _launchImp(self, url):
     """ Do launch. 
     
     Windows doesn't use the -chrome parameter.
@@ -45,25 +83,65 @@ class Win32Launcher(BrowserLauncher):
     win32api.TerminateProcess(int(self.process._handle), -1)
 
 
-class FirefoxWin32Launcher(Win32Launcher):
-  """ Launcher for firefox browser on Win32 platforms. """
+class Firefox2Win32Launcher(Win32Launcher):
+  """ Launcher for ff2 in Windows. """
+  FIREFOX_PATH = 'Mozilla Firefox\\firefox.exe'
+
   def __init__(self, profile, automated=True):
     """ Set up firefox specific variables. """
     program_files = os.getenv('PROGRAMFILES')
     self.browser_path = os.path.join(program_files, 
-                                     'Mozilla Firefox\\firefox.exe')
+                                     Firefox2Win32Launcher.FIREFOX_PATH)
     if automated:
       self.args = ['-profile', profile]
     else:
       self.args = []
+  
+
+  def launch(self, url):
+    """ Launch Firefox.
+
+    Args:
+      url: Url to launch.
+    """
+    self._startFirefox(url)
 
 
   def type(self):
-    return 'FirefoxWin32'
+    return 'Firefox2Win32'
+
+
+class Firefox3Win32Launcher(Win32Launcher):
+  """ Launcher for ff3 on Windows. """
+  FIREFOX_PATH = 'Mozilla Firefox 3 Beta 5\\firefox.exe'
+
+  def __init__(self, profile, automated=True):
+    """ Set ff3 paths. """
+    program_files = os.getenv('PROGRAMFILES')
+    self.browser_path = os.path.join(program_files,
+                                     Firefox3Win32Launcher.FIREFOX_PATH)
+    if automated:
+      self.args = ['-profile', profile]
+    else:
+      self.args = []
+  
+
+  def launch(self, url):
+    """ Launch Firefox.
+
+    Args:
+      url: Url to launch.
+    """
+    self._startFirefox(url)
+
+  
+  def type(self):
+    return 'Firefox3Win32'
 
 
 class IExploreWin32Launcher(Win32Launcher):
   """ Launcher for iexplorer browser on Win32 platforms. """
+
   def __init__(self, automated=True):
     """ Set ie specific variables. """
     program_files = os.getenv('PROGRAMFILES')
@@ -95,6 +173,7 @@ class IExploreWin32Launcher(Win32Launcher):
 
 class IExploreWinCeLauncher(BrowserLauncher):
   """ Launcher for pocket ie on Windows Mobile. """
+
   def __init__(self, automated=True):
     pass
   
@@ -119,6 +198,7 @@ class IExploreWinCeLauncher(BrowserLauncher):
 
 class FirefoxMacLauncher(BrowserLauncher):
   """ Launcher for firefox on OSX. """
+
   def __init__(self, profile, automated=True):
     """ Set firefox vars. """
     self.browser_path = '/Applications/Firefox.app/Contents/MacOS/firefox-bin'
@@ -128,12 +208,22 @@ class FirefoxMacLauncher(BrowserLauncher):
       self.args = []
   
 
+  def launch(self, url):
+    """ Launch Firefox.
+
+    Args:
+      url: Url to launch.
+    """
+    self._startFirefox(url)
+  
+
   def type(self):
     return 'FirefoxMac'
 
 
 class FirefoxLinuxLauncher(BrowserLauncher):
   """ Launcher for firefox on linux, extends BrowserLauncher. """
+
   def __init__(self, profile, automated=True):
     """ Set linux vars. """
     home = os.getenv('HOME')
@@ -146,6 +236,15 @@ class FirefoxLinuxLauncher(BrowserLauncher):
     
   def type(self):
     return 'FirefoxLinux'
+
+
+  def launch(self, url):
+    """ Launch Firefox.
+
+    Args:
+      url: Url to launch.
+    """
+    self._startFirefox(url)
 
 
   def kill(self):
