30de50681df7d806e67aae9c3bb76d4bee698193
gears.daemon <gears.daemon@fe895e04-df30-0410-9975-d76d301b4276>
2008-04-29 18:00:42 +0000

    [Author: aa]
    This change updates JsonCpp to add escaping for strings. The code is taken
    from the JsonCpp project itself, which has been fixed since we imported it.
    
    Long term, it would be better to import an up-to-date copy of JsonCpp, but
    I do not want to take on that risk right now.
    
    PRESUBMIT=passed
    R=cprince
    CC=google-gears-eng@googlegroups.com
    DELTA=82  (80 added, 0 deleted, 2 changed)
    OCL=7038147
    SCL=7052728

--- trunk/third_party/jsoncpp/json_writer.cc
+++ trunk/third_party/jsoncpp/json_writer.cc
@@ -68,7 +68,49 @@ std::string valueToString( bool value )
 
 std::string valueToQuotedString( const char *value )
 {
-   return std::string("\"") + value + "\"";
+   // Not sure how to handle unicode...
+   if (strpbrk(value, "\"\\\b\f\n\r\t") == NULL)
+      return std::string("\"") + value + "\"";
+   // We have to walk value and escape any special characters.
+   // Appending to std::string is not efficient, but this should be rare.
+   // (Note: forward slashes are *not* rare, but I am not escaping them.)
+   unsigned maxsize = strlen(value)*2 + 3; // allescaped+quotes+NULL
+   std::string result;
+   result.reserve(maxsize); // to avoid lots of mallocs
+   result += "\"";
+   for (const char* c=value; *c != 0; ++c){
+      switch(*c){
+         case '\"':
+	       result += "\\\"";
+	       break;
+	       case '\\':
+	       result += "\\\\";
+	       break;
+	       case '\b':
+	       result += "\\b";
+	       break;
+	       case '\f':
+	       result += "\\f";
+	       break;
+	       case '\n':
+	       result += "\\n";
+	       break;
+	       case '\r':
+	       result += "\\r";
+	       break;
+	       case '\t':
+	       result += "\\t";
+	       break;
+	       case '/':
+	       // Even though \/ is considered a legal escape in JSON, a bare
+	       // slash is also legal, so I see no reason to escape it.
+	       // (I hope I am not misunderstanding something.)
+	       default:
+	          result += *c;
+      }
+   }
+   result += "\"";
+   return result;
 }
 
 
