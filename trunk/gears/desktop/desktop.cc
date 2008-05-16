2834bd281736ed4436356a46e39ebf0fb432d7a1
gears.daemon <gears.daemon@fe895e04-df30-0410-9975-d76d301b4276>
2008-05-16 15:59:55 +0000

    [Author: nicolasroard]
    Adds a getter method for the context; with the latest refactoring GearsDesktop was splitted, but still do need access to the npp context in the Desktop class..
    
    PRESUBMIT=passed
    R=jripley,cprince
    CC=google-gears-eng@googlegroups.com
    DELTA=19  (19 added, 0 deleted, 0 changed)
    OCL=7188820
    SCL=7200617

--- trunk/gears/desktop/desktop.cc
+++ trunk/gears/desktop/desktop.cc
@@ -95,9 +95,16 @@ GearsDesktop::GearsDesktop()
 #endif  // OFFICIAL_BUILD
 }
 
+#ifdef OS_ANDROID
+Desktop::Desktop(const SecurityOrigin &security_origin, NPP context)
+    : security_origin_(security_origin) {
+  js_call_context_ = context;
+}
+#else
 Desktop::Desktop(const SecurityOrigin &security_origin)
     : security_origin_(security_origin) {
 }
+#endif
 
 bool Desktop::ValidateShortcutInfo(ShortcutInfo *shortcut_info) {
   // Gears doesn't allow spaces in path names, but desktop shortcuts are the
@@ -289,7 +296,11 @@ void GearsDesktop::CreateShortcut(JsCallContext *context) {
                             &shortcut_info.icon128x128.url);
 
   // Prepare the shortcut.
+#ifdef OS_ANDROID
+  Desktop desktop(EnvPageSecurityOrigin(), EnvPageJsContext());
+#else
   Desktop desktop(EnvPageSecurityOrigin());
+#endif
   if (!desktop.ValidateShortcutInfo(&shortcut_info)) {
     if (desktop.has_error())
       context->SetException(desktop.error());
