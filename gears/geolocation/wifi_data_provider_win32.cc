353c3373f97add47c96aaa2a3da8d7fffbc01f78
gears.daemon <gears.daemon@fe895e04-df30-0410-9975-d76d301b4276>
2008-06-10 20:01:18 +0000

    [Author: steveblock]
    Renames wifi_data_provider_common to wifi_data_provider_windows_common.
    
    R=andreip
    CC=gears-eng@googlegroups.com
    APPROVED=andreip
    DELTA=353  (175 added, 175 deleted, 3 changed)
    OCL=7397668
    SCL=7399127

--- gears/geolocation/wifi_data_provider_win32.cc
+++ gears/geolocation/wifi_data_provider_win32.cc
@@ -58,7 +58,7 @@
 
 #include <windows.h>
 #include "gears/base/common/string_utils.h"
-#include "gears/geolocation/wifi_data_provider_common.h"
+#include "gears/geolocation/wifi_data_provider_windows_common.h"
 
 // The time period, in milliseconds, between successive polls of the wifi data.
 static const int kPollingInterval = 1000;
