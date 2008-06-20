8d2b56fd7ff41b76dfa2294864964798190d3078
gears.daemon <gears.daemon@fe895e04-df30-0410-9975-d76d301b4276>
2008-06-20 06:04:10 +0000

    [Author: aprasath]
    Integrating codec and container libraries - speex, vorbis, ogg. Also, refactored portaudio targets into a separate rule.
    
    PRESUBMIT=passed
    R=cprince,aa
    CC=gears-eng@googlegroups.com
    APPROVED=aa
    OCL=7396213
    SCL=7496011

--- gears/tools/config.mk
+++ gears/tools/config.mk
@@ -41,6 +41,15 @@ endif
 ifeq ($(USING_ZLIB),)
   USING_ZLIB = 1
 endif
+ifeq ($(USING_PORTAUDIO),)
+  USING_PORTAUDIO = 1
+endif
+ifeq ($(USING_LIBSPEEX),)
+  USING_LIBSPEEX = 1
+endif
+ifeq ($(USING_LIBTREMOR),)
+  USING_LIBTREMOR = 1
+endif
 
 # Make-ish way of saying: if (browser == SF || browser == NPAPI)
 ifneq ($(findstring $(BROWSER), SF|NPAPI),)
@@ -187,10 +196,10 @@ ifeq ($(USING_LIBGD),1)
 CPPFLAGS += -I../third_party/libgd
 endif
 
-# TODO(vamsikrishna): change CPPFLAGS to THIRD_PARTY_CPPFLAGS, when
-# we figure out the argument ordering bug.
-# PortAudio assumes it is in the include path
-CPPFLAGS += -I../third_party/portaudio/src/common -I../third_party/portaudio/include
+PORTAUDIO_CFLAGS += -I../third_party/portaudio/src/common -I../third_party/portaudio/include
+LIBSPEEX_CFLAGS += -I../third_party/speex/include
+LIBSPEEX_CFLAGS += -DFIXED_POINT -DDISABLE_FLOAT_API -DHAVE_CONFIG_H
+LIBTREMOR_CFLAGS += -I../third_party/tremor/include -I../third_party/tremor/lib
 
 # Common items, like notifier, is not related to any browser.
 COMMON_CPPFLAGS += -DBROWSER_NONE=1
@@ -210,18 +219,19 @@ LIBGD_CFLAGS += -Wno-unused-variable -Wno-unused-function -Wno-unused-label
 SQLITE_CFLAGS += -Wno-uninitialized -DHAVE_USLEEP=1
 # for libjpeg:
 THIRD_PARTY_CFLAGS = -Wno-main
-# TODO(vamsikrishna): change CPPFLAGS to THIRD_PARTY_CPPFLAGS, when
-# we figure out the argument ordering bug.
-# PortAudio assumes it is in the include path
-CPPFLAGS += -I../third_party/portaudio/src/os/unix
+
+PORTAUDIO_CFLAGS += -I../third_party/portaudio/src/os/unix
 # for PortAudio: build only the OSS hostapi for linux
-CPPFLAGS += -DPA_USE_OSS -DHAVE_SYS_SOUNDCARD_H=1
-# TODO(vamsikrishna): change THIRD_PARTY_CFLAGS to THIRD_PARTY_CPPFLAGS, when
-# we figure out the argument ordering bug.
+PORTAUDIO_CFLAGS += -DPA_USE_OSS -DHAVE_SYS_SOUNDCARD_H=1
 # for PortAudio: disable some warnings
-THIRD_PARTY_CFLAGS += -Wno-unused-variable
-# for PortAudio: enable multithreading support with pthread library
-THIRD_PARTY_CFLAGS += -pthread
+PORTAUDIO_CFLAGS += -Wno-unused-variable
+# for PortAudio: enable multithreading support with pthread library 
+PORTAUDIO_CFLAGS += -pthread
+
+# for Speex:
+LIBSPEEX_CFLAGS += -Wno-unused-variable
+# for Tremor:
+LIBTREMOR_CFLAGS += -Wno-unused-variable -Wno-unused-function
 
 # all the GTK headers using includes relative to this directory
 GTK_CFLAGS = -I../third_party/gtk/include/gtk-2.0 -I../third_party/gtk/include/atk-1.0 -I../third_party/gtk/include/glib-2.0 -I../third_party/gtk/include/pango-1.0 -I../third_party/gtk/include/cairo -I../third_party/gtk/lib/gtk-2.0/include -I../third_party/gtk/lib/glib-2.0/include
@@ -315,19 +325,21 @@ SQLITE_CFLAGS += -Wno-uninitialized -Wno-pointer-sign -isysroot $(OSX_SDK_ROOT)
 SQLITE_CFLAGS += -DHAVE_USLEEP=1
 # for libjpeg:
 THIRD_PARTY_CFLAGS = -Wno-main
-# TODO(vamsikrishna): change CPPFLAGS to THIRD_PARTY_CPPFLAGS, when
-# we figure out the argument ordering bug.
-# PortAudio assumes it is in the include path
-CPPFLAGS += -I../third_party/portaudio/src/os/unix
+
+PORTAUDIO_CFLAGS += -I../third_party/portaudio/src/os/unix
 # for PortAudio: build only the CoreAudio hostapi for osx
-CPPFLAGS += -DPA_USE_COREAUDIO
-# TODO(vamsikrishna): change THIRD_PARTY_CFLAGS to THIRD_PARTY_CPPFLAGS, when
-# we figure out the argument ordering bug.
+PORTAUDIO_CFLAGS += -DPA_USE_COREAUDIO
 # for PortAudio: disable some warnings
-THIRD_PARTY_CFLAGS += -Wno-unused-variable -Wno-uninitialized
-# for PortAudio: enable multithreading support with pthread library
-# gcc/g++ for OSX doesn't seem to know this flag
-#THIRD_PARTY_CFLAGS += -pthread
+PORTAUDIO_CFLAGS += -Wno-unused-variable -Wno-uninitialized 
+
+# for Ogg:
+LIBOGG_CFLAGS += -D__MACOSX__
+
+# for Speex:
+LIBSPEEX_CFLAGS += -Wno-unused-variable
+
+# for Tremor:
+LIBTREMOR_CFLAGS += -Wno-unused-variable -Wno-unused-function
 
 # COMMON_CPPFLAGS affects non-browser-specific code, generated in /common.
 COMMON_CPPFLAGS += -fvisibility=hidden
@@ -428,12 +440,11 @@ CPPFLAGS_dbg = -D_DEBUG=1
 CPPFLAGS_opt =
 CPPFLAGS += /nologo -DSTRICT -D_UNICODE -DUNICODE -D_USRDLL -DWIN32 -D_WINDLL \
             -D_CRT_SECURE_NO_DEPRECATE -DNOMINMAX
-# TODO(vamsikrishna): change CPPFLAGS to THIRD_PARTY_CPPFLAGS, when
-# we figure out the argument ordering bug.
+
 # PortAudio assumes it is in the include path
-CPPFLAGS += -I../third_party/portaudio/src/os/win
+PORTAUDIO_CFLAGS += -I../third_party/portaudio/src/os/win
 # for PortAudio: build only the MME hostapi for win32/wince
-CPPFLAGS += -DPA_NO_DS -DPA_NO_ASIO
+PORTAUDIO_CFLAGS += -DPA_NO_DS -DPA_NO_ASIO
 
 ifeq ($(OS),win32)
 # We require APPVER=5.0 for things like HWND_MESSAGE.
@@ -487,10 +498,22 @@ SQLITE_CFLAGS += /wd4146
 endif
 
 THIRD_PARTY_CPPFLAGS = /wd4018 /wd4003
-# for PortAudio:
+# for PortAudio: 
 #   warning C4133: 'type' : incompatible types - from 'type1' to 'type2'
 #   warning C4101: 'identifier' : unreferenced local variable
-THIRD_PARTY_CPPFLAGS += /wd4133 /wd4101
+PORTAUDIO_CFLAGS += /wd4133 /wd4101
+
+# for Speex:
+LIBSPEEX_CFLAGS += -I../third_party/speex/win32
+#   warning C4244: conversion from 'type1' to 'type2', possible loss of data
+#   warning C4305: truncation from 'type1' to 'type2'
+LIBSPEEX_CFLAGS += /wd4244 /wd4305
+
+# for Tremor:
+#   warning C4244: conversion from 'type1' to 'type2', possible loss of data
+#   warning C4305: truncation from 'type1' to 'type2'
+#   warning C4005: macro redefinition
+LIBTREMOR_CFLAGS += /wd4244 /wd4305 /wd4005
 
 COMPILE_FLAGS_dbg = /MTd /Zi /Zc:wchar_t-
 COMPILE_FLAGS_opt = /MT  /Zi /Zc:wchar_t- /O2
