c2f4a0b66f3cee4c31d87edba9633bb87433dab7
gears.daemon <gears.daemon@fe895e04-df30-0410-9975-d76d301b4276>
2008-06-27 23:00:57 +0000

    [Author: playmobil]
    * Add step to Installer to Uninstall Initial Google Gears Release.
    * Fix clean_gears.sh script.
    
    PRESUBMIT=passed
    R=zork
    CC=gears-internal
    DELTA=7  (5 added, 1 deleted, 1 changed)
    OCL=7568380
    SCL=7569048

--- gears/tools/osx/clean_gears.sh
+++ gears/tools/osx/clean_gears.sh
@@ -2,7 +2,8 @@
 
 # Remove all files relating to Gears for Safari, except for the Gears data
 # directory in "~/Library/Application Support/Google/Gears for Safari/".
-rm -rf "~/Library/Internet Plugins/Gears.plugin"
+rm -rf '~/Library/Internet Plugins/Gears.plugin'
+sudo rm -rf "/Library/Internet Plug-Ins/Gears.plugin/"
 
 # Make sure script is run as root.
 if [ $USER != 'root' ]; then
@@ -10,7 +11,6 @@ if [ $USER != 'root' ]; then
   exit 0
 fi
 
-rm -rf "/Library/Internet Plugins/Gears.plugin"
 rm -rf "/Library/InputManagers/GearsEnabler"
 rm -rf "/Library/Receipts/Gears.pkg"
 
