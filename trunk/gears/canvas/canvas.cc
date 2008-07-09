5052d0df1707f94ee6d203744bd8181acd9843b6
gears.daemon <gears.daemon@fe895e04-df30-0410-9975-d76d301b4276>
2008-07-09 03:01:19 +0000

    [Author: kart]
    Coded more canvas functionality (resize, crop, palette), and added better test for load and export. Also eliminating reference bug where GearsCanvasRenderingContext2D keeps a plain pointer to GearsCanvas, which may get GC'ed.
    
    PRESUBMIT=passed
    R=nigeltao,satish
    CC=gears-internal
    APPROVED=nigeltao
    OCL=7549965
    SCL=7654435

--- trunk/gears/canvas/canvas.cc
+++ trunk/gears/canvas/canvas.cc
@@ -32,21 +32,33 @@
 #include "gears/canvas/canvas_rendering_context_2d.h"
 #include "third_party/skia/include/SkCanvas.h"
 #include "third_party/skia/include/SkImageDecoder.h"
+#include "third_party/skia/include/SkRect.h"
 #include "third_party/skia/include/SkStream.h"
 
 DECLARE_GEARS_WRAPPER(GearsCanvas);
 const std::string GearsCanvas::kModuleName("GearsCanvas");
 namespace {
 const SkBitmap::Config skia_config = SkBitmap::kARGB_8888_Config;
-}
+}  // namespace
 
 GearsCanvas::GearsCanvas()
     : ModuleImplBaseClassVirtual(kModuleName),
       rendering_context_(NULL),
-      skia_bitmap_(new SkBitmap) {
+      skia_bitmap_(new SkBitmap),
+      alpha_(1.0),
+      composite_operation_(STRING16(L"source-over")),
+      fill_style_(STRING16(L"#000000")),
+      font_(STRING16(L"10px sans-serif")),
+      text_align_(STRING16(L"start")) {
   // Initial dimensions as per the HTML5 canvas spec.
   skia_bitmap_->setConfig(skia_config, 300, 150);
   skia_canvas_.reset(new SkCanvas(*skia_bitmap_));
+}
+
+GearsCanvas::~GearsCanvas() {
+  // The rendering context is destroyed first, since it has a scoped_refptr to
+  // this object. See comment near bottom of canvas.h.
+  assert(rendering_context_ == NULL);
 }
 
 template<>
@@ -54,25 +66,11 @@ void Dispatcher<GearsCanvas>::Init() {
   RegisterMethod("load", &GearsCanvas::Load);
   RegisterMethod("toBlob", &GearsCanvas::ToBlob);
   RegisterMethod("clone", &GearsCanvas::Clone);
-  RegisterProperty("originalPalette", &GearsCanvas::GetOriginalPalette, NULL);
   RegisterMethod("crop", &GearsCanvas::Crop);
   RegisterMethod("resize", &GearsCanvas::Resize);
   RegisterProperty("width", &GearsCanvas::GetWidth, &GearsCanvas::SetWidth);
   RegisterProperty("height", &GearsCanvas::GetHeight, &GearsCanvas::SetHeight);
   RegisterMethod("getContext", &GearsCanvas::GetContext);
-}
-
-void GearsCanvas::LazyInitialize() {
-  if (unload_monitor_ == NULL) {
-    unload_monitor_.reset(new JsEventMonitor(GetJsRunner(), JSEVENT_UNLOAD,
-                                             this));
-  }
-}
-
-void GearsCanvas::HandleEvent(JsEventType event_type) {
-  assert(event_type == JSEVENT_UNLOAD);
-
-  rendering_context_.reset();
 }
 
 void GearsCanvas::Load(JsCallContext *context) {
@@ -108,7 +106,7 @@ void GearsCanvas::ToBlob(JsCallContext *context) {
     { JSPARAM_OPTIONAL, JSPARAM_STRING16, &mime_type },
     { JSPARAM_OPTIONAL, JSPARAM_OBJECT, &attributes }
   };
-  context->GetArguments(ARRAYSIZE(args), args);
+  int num_arguments = context->GetArguments(ARRAYSIZE(args), args);
   if (context->is_exception_set())
     return;
   SkImageEncoder::Type type;
@@ -125,13 +123,31 @@ void GearsCanvas::ToBlob(JsCallContext *context) {
     return;
   }
 
+  // TODO(kart): Figure out when is the best time to call allocPixels(),
+  // and why.
+  if (NULL == skia_bitmap_->getPixels())
+    skia_bitmap_->allocPixels();
+
   BlobBackedSkiaOutputStream output_stream;
   scoped_ptr<SkImageEncoder> encoder(SkImageEncoder::Create(type));
-  if (!encoder->encodeStream(&output_stream, *skia_bitmap_)) {
-    // TODO(kart): Support quality attribute.
+  bool encode_succeeded;
+  double quality;
+  if (num_arguments == 2 &&
+      attributes.GetPropertyAsDouble(STRING16(L"quality"), &quality)) {
+    if (quality < 0.0 || quality > 1.0) {
+      context->SetException(STRING16(L"quality must be between 0.0 and 1.0"));
+      return;
+    }
+    encode_succeeded = encoder->encodeStream(&output_stream, *skia_bitmap_,
+        static_cast<int> (quality * 100));
+  } else {
+    encode_succeeded = encoder->encodeStream(&output_stream, *skia_bitmap_);
+  }
+  if (!encode_succeeded) {
     context->SetException(STRING16(L"Could not encode image."));
     return;
   }
+
   scoped_refptr<BlobInterface> blob;
   output_stream.CreateBlob(&blob);
   scoped_refptr<GearsBlob> gears_blob;
@@ -147,10 +163,6 @@ void GearsCanvas::Clone(JsCallContext *context) {
   context->SetException(STRING16(L"Unimplemented"));
 }
 
-void GearsCanvas::GetOriginalPalette(JsCallContext *context) {
-  context->SetException(STRING16(L"Unimplemented"));
-}
-
 void GearsCanvas::Crop(JsCallContext *context) {
   int x, y, width, height;
   JsArgument args[] = {
@@ -162,27 +174,50 @@ void GearsCanvas::Crop(JsCallContext *context) {
   context->GetArguments(ARRAYSIZE(args), args);
   if (context->is_exception_set())
     return;
-  context->SetException(STRING16(L"Unimplemented"));
+  
+  SkIRect src_rect = { x, y, x + width, y + height};
+  if (!IsRectValid(src_rect)) {
+    context->SetException(STRING16(L"Invalid arguments."));
+    return;
+  }
+  SkBitmap new_bitmap;
+  new_bitmap.setConfig(skia_config, width, height);
+  SkCanvas new_canvas(new_bitmap);
+  SkRect dest_rect = { 0, 0, width, height };
+  new_canvas.drawBitmapRect(*skia_bitmap_, &src_rect, dest_rect);
+  new_bitmap.swap(*skia_bitmap_);
 }
 
 void GearsCanvas::Resize(JsCallContext *context) {
-  int width, height;
+  int new_width, new_height;
   JsArgument args[] = {
-    { JSPARAM_REQUIRED, JSPARAM_INT, &width },
-    { JSPARAM_REQUIRED, JSPARAM_INT, &height }
+    { JSPARAM_REQUIRED, JSPARAM_INT, &new_width },
+    { JSPARAM_REQUIRED, JSPARAM_INT, &new_height }
   };
   context->GetArguments(ARRAYSIZE(args), args);
   if (context->is_exception_set())
     return;
-  context->SetException(STRING16(L"Unimplemented"));
-}
-
-int GearsCanvas::Width() {
-  return skia_bitmap_->width();
-}
-
-SkScalar GearsCanvas::Height() {
-  return skia_bitmap_->height();
+  if (new_width < 0 || new_height < 0) {
+    context->SetException(STRING16(L"Cannot resize to negative dimensions."));
+    return;
+  }
+  SkBitmap new_bitmap;
+  new_bitmap.setConfig(skia_config, new_width, new_height);
+  if (Width() != 0 && Height() != 0) {
+    SkCanvas new_canvas(new_bitmap);
+    SkScalar x_scale = static_cast<SkScalar>(
+        static_cast<double>(new_width) / Width());
+    SkScalar y_scale = static_cast<SkScalar>(
+        static_cast<double>(new_height) / Height());
+    if (!new_canvas.scale(x_scale, y_scale)) {
+      context->SetException(STRING16(L"Could not resize the image."));
+      return;
+    }
+    new_canvas.drawBitmap(*skia_bitmap_, 0, 0);
+  } else {
+    new_bitmap.eraseARGB(0, 0, 0, 0);
+  }
+  new_bitmap.swap(*skia_bitmap_);
 }
 
 void GearsCanvas::GetWidth(JsCallContext *context) {
@@ -196,25 +231,27 @@ void GearsCanvas::GetHeight(JsCallContext *context) {
 }
 
 void GearsCanvas::SetWidth(JsCallContext *context) {
-  int width;
+  int new_width;
   JsArgument args[] = {
-    { JSPARAM_REQUIRED, JSPARAM_INT, &width }
+    { JSPARAM_REQUIRED, JSPARAM_INT, &new_width }
   };
   context->GetArguments(ARRAYSIZE(args), args);
   if (context->is_exception_set())
     return;
-  skia_bitmap_->setConfig(skia_config, width, Height());
+  skia_bitmap_->setConfig(skia_config, new_width, Height());
+  skia_bitmap_->eraseARGB(0, 0, 0, 0);
 }
 
 void GearsCanvas::SetHeight(JsCallContext *context) {
-  int height;
+  int new_height;
   JsArgument args[] = {
-    { JSPARAM_REQUIRED, JSPARAM_INT, &height }
+    { JSPARAM_REQUIRED, JSPARAM_INT, &new_height }
   };
   context->GetArguments(ARRAYSIZE(args), args);
   if (context->is_exception_set())
     return;
-  skia_bitmap_->setConfig(skia_config, Width(), height);
+  skia_bitmap_->setConfig(skia_config, Width(), new_height);
+  skia_bitmap_->eraseARGB(0, 0, 0, 0);
 }
 
 void GearsCanvas::GetContext(JsCallContext *context) {
@@ -230,15 +267,18 @@ void GearsCanvas::GetContext(JsCallContext *context) {
     // As per the HTML5 canvas spec.
     return;
   }
-  if (rendering_context_.get() == NULL) {
-    LazyInitialize();
+ 
+  // Make sure the rendering context is not destroyed before SetReturnValue().
+  scoped_refptr<GearsCanvasRenderingContext2D> rendering_context_scoped_ptr;
+  if (rendering_context_ == NULL) {
     if (!CreateModule<GearsCanvasRenderingContext2D>(
-            module_environment_.get(), context, &rendering_context_)) {
+        module_environment_.get(), context, &rendering_context_scoped_ptr)) {
       return;
     }
+    rendering_context_ = rendering_context_scoped_ptr.get();
     rendering_context_->InitCanvasField(this);
   }
-  context->SetReturnValue(JSPARAM_DISPATCHER_MODULE, rendering_context_.get());
+  context->SetReturnValue(JSPARAM_DISPATCHER_MODULE, rendering_context_);
 }
 
 SkBitmap *GearsCanvas::SkiaBitmap() {
@@ -247,4 +287,107 @@ SkBitmap *GearsCanvas::SkiaBitmap() {
 
 SkCanvas *GearsCanvas::SkiaCanvas() {
   return skia_canvas_.get();
-}  
+}
+
+int GearsCanvas::Width() {
+  return skia_bitmap_->width();
+}
+
+SkScalar GearsCanvas::Height() {
+  return skia_bitmap_->height();
+}
+
+double GearsCanvas::alpha() const {
+  return alpha_;
+}
+
+void GearsCanvas::set_alpha(double new_alpha) {
+  if (new_alpha >= 0.0 && new_alpha <= 1.0)
+    alpha_ = new_alpha;
+}
+
+std::string16 GearsCanvas::composite_operation() const {
+  return composite_operation_;
+}
+
+bool GearsCanvas::set_composite_operation(std::string16 new_composite_op) {
+  int op_value = ParseCompositeOperationString(new_composite_op);
+  if (op_value == COMPOSITE_MODE_HTML5_CANVAS_ONLY)
+    return false;
+  if (op_value != COMPOSITE_MODE_UNKNOWN)
+    composite_operation_ = new_composite_op;
+  return true;
+}
+
+std::string16 GearsCanvas::fill_style() const {
+  return fill_style_;
+}
+
+void GearsCanvas::set_fill_style(std::string16 new_fill_style) {
+  // TODO(kart):
+  // if (new_fill_style is not a valid CSS color)
+  //  return;
+
+  fill_style_ = new_fill_style;
+}
+
+std::string16 GearsCanvas::font() const {
+  return font_;
+}
+
+void GearsCanvas::set_font(std::string16 new_font) {
+  // TODO(kart):
+  // if (new_font is not a valid CSS font specification) {
+  //   return;
+  // }
+
+  font_ = new_font;
+}
+
+std::string16 GearsCanvas::text_align() const {
+  return text_align_;
+}
+
+void GearsCanvas::set_text_align(std::string16 new_text_align) {
+  if (new_text_align != STRING16(L"left") &&
+      new_text_align != STRING16(L"center") &&
+      new_text_align != STRING16(L"right") &&
+      new_text_align != STRING16(L"start") &&
+      new_text_align != STRING16(L"end")) {
+    return;
+  }
+  text_align_ = new_text_align;
+}
+
+bool GearsCanvas::IsRectValid(const SkIRect &rect) {
+  return rect.fLeft <= rect.fRight && rect.fTop <= rect.fBottom &&
+     rect.fLeft >= 0 && rect.fTop >= 0 &&
+     rect.fRight <= Width() && rect.fBottom <= Height();
+}
+
+bool GearsCanvas::IsRectValid(const SkRect &rect) {
+  SkIRect r = { rect.fLeft, rect.fTop, rect.fRight, rect.fBottom };
+  return IsRectValid(r);
+}
+
+// Skia's SkPorterDuff values are all non-negative.
+const int GearsCanvas::COMPOSITE_MODE_HTML5_CANVAS_ONLY = -1;
+const int GearsCanvas::COMPOSITE_MODE_UNKNOWN = -2;
+
+int GearsCanvas::ParseCompositeOperationString(std::string16 op) {
+  if (op == STRING16(L"source-atop") ||
+      op == STRING16(L"source-in") ||
+      op == STRING16(L"source-out") ||
+      op == STRING16(L"destination-atop") ||
+      op == STRING16(L"destination-in") ||
+      op == STRING16(L"destination-atop") ||
+      op == STRING16(L"lighter") ||
+      op == STRING16(L"xor")) {
+    return COMPOSITE_MODE_HTML5_CANVAS_ONLY;
+  }
+  if (op == STRING16(L"source-over"))
+    return SkPorterDuff::kSrcOver_Mode;
+  if (op == STRING16(L"copy"))
+    return SkPorterDuff::kSrc_Mode;
+  return COMPOSITE_MODE_UNKNOWN;
+}
--- trunk/gears/test/testcases/canvas_tests.js
+++ trunk/gears/test/testcases/canvas_tests.js
@@ -1,9 +1,9 @@
 // Copyright 2008, Google Inc.
 //
-// Redistribution and use in source and binary forms, with or without 
+// Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
 //
-//  1. Redistributions of source code must retain the above copyright notice, 
+//  1. Redistributions of source code must retain the above copyright notice,
 //     this list of conditions and the following disclaimer.
 //  2. Redistributions in binary form must reproduce the above copyright notice,
 //     this list of conditions and the following disclaimer in the documentation
@@ -13,21 +13,21 @@
 //     specific prior written permission.
 //
 // THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
-// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
 // MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
-// EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+// EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
 // SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
 // PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
 // OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
-// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 // ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
-// Test that properties have the correct initial values, and that they can be 
+// Test that properties have the correct initial values, and that they can be
 // read and written.
 function testProperties() {
-  var canvas = google.gears.factory.create("beta.canvas","1.0");
+  var canvas = google.gears.factory.create('beta.canvas');
   var ctx = canvas.getContext('gears-2d');
   assertEqual(300, canvas.width);
   assertEqual(150, canvas.height);
@@ -41,19 +41,19 @@ function testProperties() {
   assertEqual(WIDTH, canvas.width);
   assertEqual(HEIGHT, canvas.height);
   // TODO(kart): set to bad values and test?
- 
+
   var ALPHA = 0.4;
   ctx.globalAlpha = ALPHA;
   assertEqual(ALPHA, ctx.globalAlpha);
   ctx.globalAlpha = 9.4; // should be a no-op.
   assertEqual(ALPHA, ctx.globalAlpha);
-  
+
   var COMPOSITE_OPERATION = 'copy';
   ctx.globalCompositeOperation = COMPOSITE_OPERATION;
   assertEqual(COMPOSITE_OPERATION, ctx.globalCompositeOperation);
   ctx.globalCompositeOperation = 'foobar'; // should be a no-op.
   assertEqual(COMPOSITE_OPERATION, ctx.globalCompositeOperation);
-  
+
   var FILL_STYLE = '#34ab56';
   ctx.fillStyle = FILL_STYLE;
   assertEqual(FILL_STYLE, ctx.fillStyle);
@@ -63,7 +63,7 @@ function testProperties() {
 }
 
 function testGetContext() {
-  var canvas = google.gears.factory.create("beta.canvas","1.0");
+  var canvas = google.gears.factory.create('beta.canvas');
   var ctx = canvas.getContext('gears-2d');
   assertEqual(canvas, ctx.canvas);
   var ctx2 = canvas.getContext('gears-2d');
@@ -71,43 +71,85 @@ function testGetContext() {
   assertEqual(null, canvas.getContext('foobar'));
  }
 
-function downloadFile(url, oncomplete) {
+function assertBlobsEqual(blob1, blob2) {
+  if (isDebug)
+    assert(blob1.hasSameContentsAs(blob2));
+  else
+    assertEqual(blob1.length, blob2.length);
+}
+
+function loadBlob(filename, callback) {
   var request = google.gears.factory.create('beta.httprequest');
-  request.open('GET', url);
-  request.onreadystatechange = function() {
+  request.open('GET', '/testcases/data/' + filename);
+  request.onreadystatechange = function(){
     if (request.readyState == 4) {
-      oncomplete(request.responseBlob);
+      callback(filename, request.responseBlob);
     }
   };
   request.send();
 }
 
-function processDownloadedImage(blob) {
-  var canvas = google.gears.factory.create("beta.canvas","1.0");
-  canvas.load(blob);
-  var pngBlob = canvas.toBlob('image/png');
-  var jpegBlob = canvas.toBlob('image/jpeg');
-  var defaultBlob = canvas.toBlob();
+// Loads a bunch of blobs and when all of them are loaded, calls the callback
+// passing a map from filename to blob.
+function loadBlobs(filenames, callback) {
+  var numLoaded = 0;
+  var blobs = {};
+
+  for (var i = 0; i < filenames.length; ++i) {
+    loadBlob(filenames[i], function(filename, blob) {
+      blobs[filename] = blob;
+      if (++numLoaded == filenames.length) {
+        callback(blobs);
+      }
+    });
+  }
+}
+
+function runLoadAndExportTest(blobs) {
+  var formats = ['png', 'jpeg'];
+  for (var i = 0; i < formats.length; ++i) {
+    var format = formats[i];
+    var canvas = google.gears.factory.create('beta.canvas');  
+    canvas.load(blobs['sample-original.' + format]);
+    
+    var pngBlob = canvas.toBlob('image/png');
+    // The default format is png.
+    assertBlobsEqual(canvas.toBlob(), pngBlob);
+    var jpegBlob = canvas.toBlob('image/jpeg');
+    var jpegLowBlob = canvas.toBlob('image/jpeg', { quality: 0.02 });
+    // Now compare the exported versions with golden files.
+    // The golden files have been manually checked for format and size.
+    assertBlobsEqual(pngBlob, 
+        blobs['sample-' + format + '-exported-as-png.png']);
+    assertBlobsEqual(jpegBlob, 
+        blobs['sample-' + format + '-exported-as-jpeg.jpeg']);
+    assertBlobsEqual(jpegLowBlob,
+        blobs['sample-' + format + '-exported-as-jpeg-with-quality-0.02.jpeg']);
+  }
   completeAsync();
 }
 
-function testLoadAndExportJPEG() {
+function testLoadAndExport() {
   startAsync();
-  downloadFile('../testcases/data/sample.jpeg', processDownloadedImage);
-}
-
-function testLoadAndExportPNG() {
-  startAsync();
-  downloadFile('../testcases/data/sample.png', processDownloadedImage);
+  var filenames = [
+    'sample-original.jpeg',
+    'sample-original.png',
+    'sample-jpeg-exported-as-jpeg-with-quality-0.02.jpeg',
+    'sample-jpeg-exported-as-jpeg.jpeg',
+    'sample-jpeg-exported-as-png.png',
+    'sample-png-exported-as-jpeg-with-quality-0.02.jpeg',
+    'sample-png-exported-as-jpeg.jpeg',
+    'sample-png-exported-as-png.png'];
+  loadBlobs(filenames, runLoadAndExportTest);
 }
 
 /*
 function testDrawImage() {
   if(isOfficial)
     return;
-  var canvas = google.gears.factory.create("beta.canvas","1.0");
+  var canvas = google.gears.factory.create('beta.canvas');
   var ctx = canvas.getContext('gears-2d');
-  var canvas2 = google.gears.factory.create("beta.canvas","1.0");
+  var canvas2 = google.gears.factory.create('beta.canvas');
   // assertError(ctx.drawImage(canvas2, 1000, 1000, 30, 20, 0, 0, 10, 10));
     // TODO(kart): check for INDEX_SIZE_ERR
   // assertError(ctx.drawImage(42));
@@ -117,24 +159,24 @@ function testDrawImage() {
 
 // TODO(kart): restore should do nothing if the stack is
 // empty (i.e, blank canvas)
- 
+
 // TODO(kart): clearRect and fillRect have no effect is width or height is zero.
 // strokeRect has no effect if both width and height are zero, and draws a line
 // if exactly one of width and height is zero.
- 
+
 // TODO(kart): If any of the arguments to createImageData() or getImageData()
 // are infinite or NaN, or if either the sw or sh arguments are zero, the method
 // must instead raise an INDEX_SIZE_ERR exception.
- 
+
 // TODO(kart): The values of the data array may be changed
 // (the length of the array,
-//  and the other attributes in ImageData objects, are all read-only). 
+//  and the other attributes in ImageData objects, are all read-only).
 // On setting, JS undefined values must be converted to zero. Other values must
 // first be converted to numbers using JavaScript's ToNumber algorithm, and if
 // the result is a NaN value, a TYPE_MISMATCH_ERR exception must be raised. If
 // the result is less than 0, it must be clamped to zero. If the result is more
 // than 255, it must be clamped to 255. If the number is not an integer, it must
-//  be rounded to the nearest integer using the IEEE 754r roundTiesToEven 
+//  be rounded to the nearest integer using the IEEE 754r roundTiesToEven
 // rounding mode.
 
 // TODO(kart): putImageData: If the first argment to the method is null or
@@ -144,6 +186,6 @@ function testDrawImage() {
 // any of the arguments to the method are infinite or NaN, the method must raise
 // an INDEX_SIZE_ERR exception.
 
-// TODO(kart): make sure 
+// TODO(kart): make sure
 // context.putImageData(context.getImageData(x, y, w, h), x, y);
 // is a noop.
