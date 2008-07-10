7c7e6d4bc6b36761fb0fb788633b2be9501d6fe2
gears.daemon <gears.daemon@fe895e04-df30-0410-9975-d76d301b4276>
2008-07-10 19:03:26 +0000

    [Author: fry]
    - generate large cgi responses rapidly
    - cleanup and expand manual progress test display page
    
    PRESUBMIT=passed
    R=bgarcia
    CC=gears-eng@googlegroups.com
    APPROVED=playmobil
    DELTA=83  (48 added, 14 deleted, 21 changed)
    OCL=7670557
    SCL=7673435

--- trunk/gears/test/testcases/cgi/send_response_of_size.py
+++ trunk/gears/test/testcases/cgi/send_response_of_size.py
@@ -4,8 +4,5 @@ self.send_response(200)
 self.send_header('Content-Type', 'text/html')
 self.send_header('Content-Length', size)
 self.end_headers()
-  
-for i in xrange(0, size):
-  self.outgoing.append('a')
-else:
-  self.outgoing.append(None)
+
+self.outgoing.append('a'*size)
