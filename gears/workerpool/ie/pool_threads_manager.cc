af42669ee8ee1d308039f56544ac84ef6811b18d
gears.daemon <gears.daemon@fe895e04-df30-0410-9975-d76d301b4276>
2008-07-23 19:02:28 +0000

    [Author: mpcomplete]
    Use Event (with CondVar) instead of Condition for a few long-lasting conditions.  This way they don't busy wait and eat CPU.
    
    BUG=1280317
    R=bgarcia
    CC=gears-internal
    DELTA=137  (5 added, 62 deleted, 70 changed)
    OCL=7753819
    SCL=7787953

--- gears/workerpool/ie/pool_threads_manager.cc
+++ gears/workerpool/ie/pool_threads_manager.cc
@@ -47,6 +47,7 @@
 #include "gears/workerpool/ie/pool_threads_manager.h"
 
 #include "gears/base/common/atomic_ops.h"
+#include "gears/base/common/event.h"
 #include "gears/base/common/module_wrapper.h"
 #include "gears/base/common/js_runner.h"
 #include "gears/base/common/scoped_win32_handles.h"
@@ -95,8 +96,8 @@ struct JavaScriptWorkerInfo {
   JavaScriptWorkerInfo()
       : threads_manager(NULL), js_runner(NULL), is_owning_worker(false),
         message_hwnd(0), is_invoking_error_handler(false),
-        thread_init_signalled(false), thread_init_ok(false),
-        script_signalled(false), script_ok(false),
+        thread_init_ok(false),
+        script_ok(false),
         is_factory_suspended(false), thread_handle(INVALID_HANDLE_VALUE) {}
 
   ~JavaScriptWorkerInfo() {
@@ -125,12 +126,10 @@ struct JavaScriptWorkerInfo {
   //
   // These fields are used only for created workers (children).
   //
-  Mutex thread_init_mutex;  // Protects: thread_init_signalled
-  bool thread_init_signalled;
+  Event thread_init_event;
   bool thread_init_ok;  // Owner: child before signal, parent after signal
 
-  Mutex script_mutex;  // Protects: script_signalled
-  bool script_signalled;
+  Event script_event;
   bool script_ok;  // Owner: parent before signal, immutable after
   std::string16 script_text;  // Owner: parent before signal, immutable after
   SecurityOrigin script_origin;  // Owner: parent before signal, immutable after
@@ -497,8 +496,8 @@ class CreateWorkerUrlFetchListener : public HttpRequest::HttpListener {
         // Must use security origin of final url, in case there were redirects.
         wi_->script_origin.InitFromUrl(final_url.c_str());
       } else {
-        // Throw an error, but don't return!  Continue and set script_signalled
-        // so the worker doesn't spin forever in Mutex::Await().
+        // Throw an error, but don't return!  Continue and signal script_event
+        // so the worker doesn't wait forever in Event::Wait().
         std::string16 message(STRING16(L"Failed to load script."));
         std::string16 status_line;
         if (source->GetStatusLine(&status_line)) {
@@ -514,10 +513,7 @@ class CreateWorkerUrlFetchListener : public HttpRequest::HttpListener {
         wi_->threads_manager->HandleError(error_info);
       }
 
-      wi_->script_mutex.Lock();
-      assert(!wi_->script_signalled);
-      wi_->script_signalled = true;
-      wi_->script_mutex.Unlock();
+      wi_->script_event.Signal();
     }
   }
  private:
@@ -567,10 +563,7 @@ bool PoolThreadsManager::CreateThread(const std::string16 &url_or_full_script,
     wi->script_ok = true;
     wi->script_text += url_or_full_script;
     wi->script_origin = page_security_origin_;
-
-    wi->script_mutex.Lock();
-    wi->script_signalled = true;
-    wi->script_mutex.Unlock();
+    wi->script_event.Signal();
   } else {
     // For URL params we start an async fetch here.  The created thread will
     // setup an incoming message queue, then Mutex::Await for the script to be
@@ -595,13 +588,11 @@ bool PoolThreadsManager::CreateThread(const std::string16 &url_or_full_script,
       return false;
     }
 
-    // 'script_signalled' will be set when async fetch completes.
+    // 'script_event.Signal()' will be called when async fetch completes.
   }
 
   // Setup notifier to know when thread init has finished.
   // Then create thread and wait for signal.
-  wi->thread_init_mutex.Lock();
-  wi->thread_init_signalled = false;
 
   wi->thread_handle.reset((HANDLE)_beginthreadex(
       NULL,                   // security descriptor
@@ -610,13 +601,11 @@ bool PoolThreadsManager::CreateThread(const std::string16 &url_or_full_script,
       wi,                     // argument pointer
       0,                      // initial state (0 for running)
       NULL));                 // variable to receive thread ID
+
   if (wi->thread_handle != 0) {  // 0 means thread creation error
     // thread needs to finish message queue init before we continue
-    wi->thread_init_mutex.Await(Condition(&wi->thread_init_signalled));
+    wi->thread_init_event.Wait();
   }
-
-  // cleanup notifier
-  wi->thread_init_mutex.Unlock();
 
   if (wi->thread_handle == 0 || !wi->thread_init_ok) {
     return false;  // failed
@@ -636,7 +625,7 @@ unsigned __stdcall PoolThreadsManager::JavaScriptThreadEntry(void *args) {
   // Setup worker thread.
   // Then signal that initialization is done, and indicate success/failure.
   //
-  // WARNING: must fire thread_init_signalled even on failure, or caller won't
+  // WARNING: must fire thread_init_event even on failure, or caller won't
   // continue.  So fire it from a non-nested location, before any early exits.
   scoped_ptr<JsRunnerInterface> js_runner(NewJsRunner());
   assert(NULL == wi->js_runner);
@@ -646,16 +635,12 @@ unsigned __stdcall PoolThreadsManager::JavaScriptThreadEntry(void *args) {
                                wi->threads_manager->InitWorkerThread(wi);
 
   wi->thread_init_ok = thread_init_succeeded;
-  wi->thread_init_mutex.Lock();
-  wi->thread_init_signalled = true;
-  wi->thread_init_mutex.Unlock();
+  wi->thread_init_event.Signal();
 
   if (thread_init_succeeded) {
-    // Block until 'script_signalled' (i.e. wait for URL fetch, if being used).
-    // Thread shutdown will set this flag as well.
-    wi->script_mutex.Lock();
-    wi->script_mutex.Await(Condition(&wi->script_signalled));
-    wi->script_mutex.Unlock();
+    // Block until 'script_event' is signaled (i.e. wait for URL fetch, if
+    // being used). Thread shutdown will set this flag as well.
+    wi->script_event.Wait();
 
     if (wi->script_ok) {
       if (SetupJsRunner(js_runner.get(), wi)) {
@@ -911,10 +896,8 @@ void PoolThreadsManager::ShutDown() {
 
       // If the worker is a created thread...
       if (wi->thread_handle != INVALID_HANDLE_VALUE) {
-        // Ensure the thread isn't waiting on 'script_signalled'.
-        wi->script_mutex.Lock();
-        wi->script_signalled = true;
-        wi->script_mutex.Unlock();
+        // Ensure the thread isn't waiting on 'script_event'.
+        wi->script_event.Signal();
 
         // Ensure the thread sees 'is_shutting_down_' by sending a dummy
         // message, in case it is blocked waiting for messages.
