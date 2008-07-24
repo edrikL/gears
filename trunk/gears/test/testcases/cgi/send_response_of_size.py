7f91eddaf5c661553945afffcae30905db4ef44a
gears.daemon <gears.daemon@fe895e04-df30-0410-9975-d76d301b4276>
2008-07-24 23:02:14 +0000

    [Author: ace]
    Modify block response script to send 16k chunks in a loop instead of one huge block.
    This change is also a test to try and solve intermittent failures in testAbort
    httprequest tests.
    
    R=michaeln
    CC=gears-eng@googlegroups.com
    APPROVED=playmobil
    DELTA=14  (13 added, 0 deleted, 1 changed)
    OCL=7789957
    SCL=7804057

--- trunk/gears/test/testcases/cgi/send_response_of_size.py
+++ trunk/gears/test/testcases/cgi/send_response_of_size.py
@@ -5,4 +5,17 @@ self.send_header('Content-Type', 'text/html')
 self.send_header('Content-Length', size)
 self.end_headers()
 
-self.outgoing.append('a'*size)
+# Send data in blocks.  Less memory intensive than allocating
+# and sending one big string, but faster than sending one 
+# byte at a time.  This is a compromise, because there are some
+# tests that want to send large quantities of data quickly, 
+# and others that want to interact with the httprequest object
+# during transfer.
+block_size = 1024 * 16
+num_blocks = size / block_size
+block_data = 'a' * block_size
+remainder = 'a' * (size % block_size)
+
+for i in xrange(num_blocks):
+  self.outgoing.append(block_data)
+self.outgoing.append(remainder)
