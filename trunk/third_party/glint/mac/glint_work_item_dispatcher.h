2efc86c9b397e156af31aa203ac06a4a18feab62
gears.daemon <gears.daemon@fe895e04-df30-0410-9975-d76d301b4276>
2008-08-05 23:03:18 +0000

    [Author: dimich]
    Reflect changes in Gllint (SVN revision 48)
    Add Autorelease pool to logging functions since they are called from C++ worker threads.
    
    PRESUBMIT=passed
    R=grobbins
    CC=gears-eng@googlegroups.com
    APPROVED=grobbins
    DELTA=164  (71 added, 39 deleted, 54 changed)
    OCL=7879073
    SCL=7904610

--- trunk/third_party/glint/mac/glint_work_item_dispatcher.h
+++ trunk/third_party/glint/mac/glint_work_item_dispatcher.h
@@ -33,23 +33,43 @@
 using glint::WorkItem;
 using glint::RootUI;
 
+// Used to hold Glint WorkItem and manage its execution.
+// When Glint creates a Workitem for async execution on UI thread, an instance
+// of this class is created, WorkItem pointer stored in it and it is registered
+// with main UI thread message pump by means of performSelectorOnMainThread.
+// This will eventually cause 'dispatch' selector to be invoked by message pump.
+// We also keep a reference to this object in a global list attached to Glint
+// window, to 'cancel' all scheduled but not yet executed work items when the
+// window is closed.
 @interface GlintWorkItemDispatcher : NSObject {
-  RootUI* ui_;  // weak
-  WorkItem* workItem_;  // weak
+  NSMutableSet* dispatchers_;  // weak reference to containing list.
+  RootUI* ui_;  // weak C++ reference.
+  WorkItem* workItem_;  // strong C++ reference, we release it.
+  BOOL isCanceled_;
 }
 
-// Creates an autoreleased GlintWorkItemDispatcher for the given work item for
-// the given Glint ui
+// Creates a GlintWorkItemDispatcher for the given work item for
+// the given Glint ui.
 + (GlintWorkItemDispatcher*)dispatcherForWorkItem:(WorkItem*)item
-                                           withUI:(RootUI*)ui;
+                                           withUI:(RootUI*)ui
+                                        container:(NSMutableSet*)dispatchers;
 
 // Designated initializer, initializes the dispatcher with the given work item
-// for the given Glint ui
+// for the given Glint ui.
 - (id)initForWorkItem:(WorkItem*)item
-               withUI:(RootUI*)ui;
+               withUI:(RootUI*)ui
+            container:(NSMutableSet*)dispatchers;
 
 // Dispatches the work item on the Glint ui it was created for. This is never
 // called directly. It only serves as a target for a run loop.
 - (void)dispatch;
 
+// Once dispatcher is queued for a callback on UI thread, it is not always
+// possible to cancel the callback itself. This method marks dispatcher
+// 'canceled' so subsequent callback is a noop.
+- (void)cancel;
+
 @end
+
+
+
