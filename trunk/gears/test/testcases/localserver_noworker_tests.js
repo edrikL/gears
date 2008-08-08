e94b8b121aa68d63ef13106c69968d94143871fc
gears.daemon <gears.daemon@fe895e04-df30-0410-9975-d76d301b4276>
2008-08-08 22:00:55 +0000

    [Author: playmobil]
    Only run CaptureFile tests in IE & Firefox.
    
    PRESUBMIT=passed
    R=mpcomplete
    CC=gears-eng@googlegroups.com
    APPROVED=aa
    DELTA=92  (46 added, 46 deleted, 0 changed)
    OCL=7932453
    SCL=7941628

--- trunk/gears/test/testcases/localserver_noworker_tests.js
+++ trunk/gears/test/testcases/localserver_noworker_tests.js
@@ -1,44 +1,6 @@
 var localServer = google.gears.factory.create('beta.localserver');
 var resourceStore = localServer.createStore('noworker_tests');
 
-function testCreateFileSubmitter() {
-  assertNotNull(resourceStore.createFileSubmitter(),
-                'Could not create FileSubmitter');
-}
-
-function testCaptureEmptyFileElement() {
-  var div = document.createElement('div');
-  document.body.appendChild(div);
-  div.innerHTML = '<input type="file">';
-  var input = document.getElementsByTagName('input')[0];
-  assertError(function() {
-    resourceStore.captureFile(input, 'should_fail');
-    }, 'File path is empty.',
-    'Expected capturing empty file input element to fail');
-}
-
-function testCaptureSpoofedInputElement() {
-  // QueryInterface is a Firefox-specific thing, but this test is also useful
-  // for IE.
-  var fileInputSpoofer = {
-    type: "file",
-    value: "c:\\autoexec.bat",
-    QueryInterface: function(iid) {
-      return this;
-    }
-  }
-
-  assertError(function() {
-    resourceStore.captureFile(fileInputSpoofer, 'foo');
-  });
-}
-
-function testCaptureFilenameString() {
-  assertError(function() {
-    resourceStore.captureFile('c:\\autoexec.bat', 'bar');
-  });
-}
-
 function testCaptureLongUrl() {
 
   var storeName = 'maximalUserSuppliedStoreNameWhichCanGetVeryLongAsYouSeeaaaaaaaaa';
@@ -85,11 +47,3 @@ function testCaptureLongUrl() {
     completeAsync();      
   });
 }
-
-// SAFARI-TEMP - Disable tests that don't currently work on Safari.
-testCreateFileSubmitter._disable_in_safari = true;
-testCaptureEmptyFileElement._disable_in_safari = true;
-
-// NPAPI-TEMP - Disable tests that don't currently work in npapi build.
-testCreateFileSubmitter._disable_in_npapi = true;
-testCaptureEmptyFileElement._disable_in_npapi = true;
