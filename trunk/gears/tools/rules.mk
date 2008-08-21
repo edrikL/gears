04470b2870b1dfb9946b5cf9b6a9dce6bcff5511
gears.daemon <gears.daemon@fe895e04-df30-0410-9975-d76d301b4276>
2008-08-21 23:01:14 +0000

    [Author: jianli]
    Use Breakpad to report crash for Notifier in MacOSX platform.
    
    PRESUBMIT=passed
    R=playmobil
    CC=gears-eng@googlegroups.com
    APPROVED=playmobil
    DELTA=47  (40 added, 0 deleted, 7 changed)
    OCL=8063007
    SCL=8063776

--- trunk/gears/tools/rules.mk
+++ trunk/gears/tools/rules.mk
@@ -929,8 +929,12 @@ $(NOTIFIER_DLL): $(NOTIFIER_OBJS) $(BREAKPAD_OBJS) $(NOTIFIER_LINK_EXTRAS)
 	$(MKDLL) $(DLLFLAGS) $(NOTIFIER_LINK_EXTRAS) $(NOTIFIER_LIBS) $(EXT_LINKER_CMD_FLAG)$(OUTDIR)/obj_list.temp
 	rm $(OUTDIR)/obj_list.temp
 else
-$(NOTIFIER_EXE): $(NOTIFIER_OBJS) $(NOTIFIER_LINK_EXTRAS)
-	$(MKEXE) $(EXEFLAGS) $(NOTIFIER_OBJS) $(NOTIFIER_LINK_EXTRAS) $(NOTIFIER_LIBS)
+$(NOTIFIER_EXE): $(NOTIFIER_OBJS) $(BREAKPAD_OBJS) $(NOTIFIER_LINK_EXTRAS)
+	$(MKEXE) $(EXEFLAGS) $(NOTIFIER_OBJS) $(BREAKPAD_OBJS) $(NOTIFIER_LINK_EXTRAS) $(NOTIFIER_LIBS)
+# Dump the symbols and strip the executable
+	../third_party/breakpad_osx/src/tools/mac/dump_syms/dump_syms -a ppc $@ > $@_ppc.symbols
+	../third_party/breakpad_osx/src/tools/mac/dump_syms/dump_syms -a i386 $@ > $@_i386.symbols
+	$(STRIP_EXECUTABLE)
 endif
 
 $(NOTIFIER_PREFPANE_EXE): $(NOTIFIER_PREFPANE_OBJS)
