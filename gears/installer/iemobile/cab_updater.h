ae6d23553462d9e0b92f7abd11a995c627795ffc
gears.daemon <gears.daemon@fe895e04-df30-0410-9975-d76d301b4276>
2008-10-20 23:01:23 +0000

    [Author: steveblock]
    Factors out browser-independent parts of CabUpdater for WinCE.
    
    R=andreip
    CC=gears-eng@googlegroups.com
    DELTA=1046  (594 added, 434 deleted, 18 changed)
    OCL=8606512
    SCL=8662533

--- gears/installer/iemobile/cab_updater.h
+++ gears/installer/iemobile/cab_updater.h
@@ -22,45 +22,37 @@
 // WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
 // OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 // ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// This file defines the CabUpdater class, which uses the PeriodicChecker to
+// handle automitic updating.
+//
+// 1. When the Gears BHO is loaded, the CabUpdater is started immediately after
+// the HTTP handler is registered. The PeriodicChecker is configured to check 
+// for an update after two minutes and every 24 hours thereafter. The
+// PeriodicChecker is started.
+// 2. If the PeriodicChecker reports a new version, we present the user with a
+// dialog, asking if they would like to upgrade.
+// 3. If the user agrees, the CabUpdater uses the IWebBrowser2 pointer to make
+// the browser download the new CAB.
+// 4. The CabUpdater waits for a new update from the PeriodicChecker and
+// continues from step 2.
 
 #ifndef GEARS_INSTALLER_IEMOBILE_CAB_UPDATER_H__
 #define GEARS_INSTALLER_IEMOBILE_CAB_UPDATER_H__
 
-#include <windows.h>
 #include <piedocvw.h>
 
 #include "gears/base/common/common.h"
 #include "gears/base/common/message_service.h"
-#include "gears/base/common/sqlite_wrapper.h"
-#include "gears/base/ie/atl_headers.h"
-#include "gears/localserver/common/async_task.h"
-#include "third_party/scoped_ptr/scoped_ptr.h"
+#include "gears/installer/iemobile/periodic_checker.h"
 
-// This class takes care of periodic version updates. It does this as follows:
-// 1. When the Gears BHO is loaded, CAB updater is started immediately after
-// the HTTP handler is registered.
-// 2. The CAB updater sets up a timer and goes to sleep for some time
-// 3. When the CAB updater wakes up, it does a HTTP GET to the upgrade URL
-// and aborts after the first redirect.
-// 4. When the request completes, the updater inspects the Location header
-// and extracts the latest version number.
-// 5. The CAB updater checks the latest version number vs. the current version
-// number. If the two are different,
-// 6. The CAB updater asks the user if it should proceed with the upgrade. If
-// the user agrees,
-// 7. The CAB updater uses the IWebBrowser2 pointer to make the browser
-// download the new CAB.
-// 8. The updater waits for 24 hours, then continues at step 3.
-
-// Forward declarations
-class PeriodicChecker;
-
-class CabUpdater : public MessageObserverInterface {
+class CabUpdater
+    : public MessageObserverInterface,
+      public PeriodicChecker::ListenerInterface {
  public:
   CabUpdater();
   virtual ~CabUpdater();
-  // Starts the updater. Sleep a few seconds and work out if an update check
-  // is due.
+  // Starts the updater.
   void SetSiteAndStart(IWebBrowser2* browser);
 
   // MessageObserverInterface
@@ -77,6 +69,9 @@ class CabUpdater : public MessageObserverInterface {
   static void Stop(void* self);
   // Shows the update dialog to the user.
   bool ShowUpdateDialog(HWND browser_window);
+  // PeriodicChecker::ListenerInterface implementation
+  virtual void UpdateUrlAvailable(const std::string16 &url);
+
   // We drive the browser through this IWebBrowser2 pointer. Not owned.
   IWebBrowser2* browser_;
   // The periodic update checker. Owned.
