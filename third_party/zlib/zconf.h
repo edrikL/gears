da04cfa5e69dd0040ad3e7dc21d214692317da68
gears.daemon <gears.daemon@fe895e04-df30-0410-9975-d76d301b4276>
2008-12-19 00:03:20 +0000

    [Author: iyengar]
    Attempt 2 at landing this. 
    
    Consolidating the zlib revisions used by Gears and Chrome
    
    PRESUBMIT=passed
    APPROVED=mpcomplete
    OCL=9435507
    SCL=9472940

--- third_party/zlib/zconf.h
+++ third_party/zlib/zconf.h
@@ -3,10 +3,13 @@
  * For conditions of distribution and use, see copyright notice in zlib.h
  */
 
-/* @(#) $Id$ */
+/* @(#) $Id: zconf.h,v 3.9 2005/08/04 19:14:14 tor%cs.brown.edu Exp $ */
 
 #ifndef ZCONF_H
 #define ZCONF_H
+
+/* This include does prefixing as below, but with an updated set of names */
+#include "mozzconf.h"
 
 /*
  * If you *really* need a unique prefix for all types and library functions,
