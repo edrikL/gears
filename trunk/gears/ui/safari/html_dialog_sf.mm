402c3099da8c12895f5fabc331b09555d94a86e9
gears.daemon <gears.daemon@fe895e04-df30-0410-9975-d76d301b4276>
2009-01-06 22:59:58 +0000

    [Author: playmobil]
    Fix for bug 739 - Correctly retrieve language from International Preferences rather
    than from Format/Region.
    
    PRESUBMIT=passed
    R=zork
    CC=gears-eng@googlegroups.com
    DELTA=24  (23 added, 1 deleted, 0 changed)
    OCL=9617418
    SCL=9617527

--- trunk/gears/ui/safari/html_dialog_sf.mm
+++ trunk/gears/ui/safari/html_dialog_sf.mm
@@ -1,9 +1,9 @@
 // Copyright 2008, Google Inc.
 //
-// Redistribution and use in source and binary forms, with or without 
+// Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
 //
-//  1. Redistributions of source code must retain the above copyright notice, 
+//  1. Redistributions of source code must retain the above copyright notice,
 //     this list of conditions and the following disclaimer.
 //  2. Redistributions in binary form must reproduce the above copyright notice,
 //     this list of conditions and the following disclaimer in the documentation
@@ -13,14 +13,14 @@
 //     specific prior written permission.
 //
 // THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
-// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
 // MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
-// EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+// EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
 // SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
 // PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
 // OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
-// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 // ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #import <Cocoa/Cocoa.h>
@@ -45,7 +45,7 @@
 static NSDictionary *ScriptableMethods() {
   // In order to create a static objective-c object we need to create it after
   // the runtime is initialized.
-  static const NSDictionary *scriptable_methods = 
+  static const NSDictionary *scriptable_methods =
       [[NSDictionary dictionaryWithObjectsAndKeys:
                          @"setResults",
                          @"setResults:",
@@ -60,7 +60,7 @@ static NSDictionary *ScriptableMethods() {
 - (BOOL)createWindow:(unsigned int)window_style {
   // Create the dialog's window.
   NSRect content_rect = NSMakeRect(0, 0, width_, height_);
-  window_ = [[NSWindow alloc]  initWithContentRect:content_rect 
+  window_ = [[NSWindow alloc]  initWithContentRect:content_rect
                                          styleMask:window_style
                                            backing:NSBackingStoreBuffered
                                              defer:YES];
@@ -68,25 +68,25 @@ static NSDictionary *ScriptableMethods() {
   if (!window_) {
     return false;
   }
-  
+
   [window_ setMinSize:NSMakeSize(width_, height_)];
   [window_ setReleasedWhenClosed:YES];
-  
+
   // Create a WebView and attach it.
   NSView *content_view = [window_ contentView];
   WebView *webview = [[WebView alloc] initWithFrame:[content_view frame]];
-  
+
   // Set the user agent for the WebView that we're opening to the same one as
   // Safari.
   std::string16 ua_str16;
   BrowserUtils::GetUserAgentString(&ua_str16);
-  
+
   // The user agent string is initialized by the plugin's NP_Initialize
   // function, but the settings dialog may be displayed before that's
   // been called, in which case we just use a default string.
   NSURL *tmp_url = [NSURL URLWithString:@""];
   user_agent_ = [NSString stringWithString16:ua_str16.c_str()];
-  if (ua_str16.length() == 0) { 
+  if (ua_str16.length() == 0) {
     user_agent_ = [NSString stringWithFormat:@"%@ Safari",
                     [webview userAgentForURL:tmp_url]];
   }
@@ -96,14 +96,14 @@ static NSDictionary *ScriptableMethods() {
   [content_view addSubview:webview];
   [webview release]; // addSubView retains webView.
   [webview setAutoresizingMask:NSViewHeightSizable | NSViewWidthSizable];
-  
+
   // Make ourselves the WebView's delegate.
   [webview setFrameLoadDelegate:self];
   [webview setUIDelegate:self];
-  
+
   // Load in the dialog's contents.
   WebFrame *frame = [webview mainFrame];
-  
+
   // Read in WebArchive.
   unsigned char *resource_data;
   size_t resource_len;
@@ -114,11 +114,11 @@ static NSDictionary *ScriptableMethods() {
                          &resource_len)) return false;
   NSData *archive_data = [NSData dataWithBytes:resource_data
                                         length:resource_len];
-  
+
   WebArchive *webarchive = [[WebArchive alloc] initWithData:archive_data];
-  
+
   [frame loadArchive:webarchive];
-  
+
   // Turn off scrollbars.
   [[frame frameView] setAllowsScrolling:NO];
 
@@ -126,7 +126,7 @@ static NSDictionary *ScriptableMethods() {
 }
 
 #pragma mark Public Instance methods
-- (id)initWithHtmlFile:(const std::string16 &)html_filename 
+- (id)initWithHtmlFile:(const std::string16 &)html_filename
              arguments:(const std::string16 &)arguments
                  width:(int)width
                 height:(int)height {
@@ -159,30 +159,30 @@ static NSDictionary *ScriptableMethods() {
   if (![self createWindow:window_style]) {
     return false;
   }
-  
+
   // See gears/base/safari/safari_workarounds.m for details.
   EnableWebkitTimersForNestedRunloop();
 
   // Display the dialog.
   NSWindow *front_window = [NSApp keyWindow];
-  [NSApp beginSheet:window_ 
-     modalForWindow:front_window 
-      modalDelegate:nil 
+  [NSApp beginSheet:window_
+     modalForWindow:front_window
+      modalDelegate:nil
      didEndSelector:nil
         contextInfo:nil];
- 
+
  // Spin until the sheet is closed.
  // Credit goes to David Sinclair of Dejal software for this method of running
  // a modal WebView.
  NSModalSession session = [NSApp beginModalSessionForWindow:window_];
  NSRunLoop *runloop = [NSRunLoop currentRunLoop];
  NSDate *oneHunderMS = [NSDate dateWithTimeIntervalSinceNow:0.1];
-  while (!window_dismissed_ && 
+  while (!window_dismissed_ &&
           [NSApp runModalSession:session] == NSRunContinuesResponse) {
      [runloop runMode:NSDefaultRunLoopMode beforeDate:oneHunderMS];
   }
   [NSApp endModalSession:session];
-  
+
 // Display Document-Modal dialog, this doesn't work on 10.4.
 //  while (!window_dismissed_) {
 //    [NSApp setWindowsNeedUpdate:YES];
@@ -191,25 +191,25 @@ static NSDictionary *ScriptableMethods() {
 //                                           inMode:NSDefaultRunLoopMode
 //                                          dequeue:YES];
 //    [NSApp sendEvent:event];
-//  }  
-  
+//  }
+
   [NSApp endSheet:window_];
   [window_ close];
   window_ = nil;
-  
+
   [result_string_ string16:results];
   return true;
 }
 
 // Delegate method called just before any js is run, so this is the perfect
 // time to inject vairables into the js environment.
-- (void)webView:(WebView *)webView 
+- (void)webView:(WebView *)webView
             windowScriptObjectAvailable:(WebScriptObject *)windowScriptObject {
-      
+
   // Pass in dialog arguments in the gears_dialogArguments variable.
   [windowScriptObject setValue:arguments_
                         forKey:@"gears_dialogArguments"];
-  
+
   // Register ourselves as the window.gears_dialog object.
   [windowScriptObject setValue:self forKey:@"gears_dialog"];
 }
@@ -230,8 +230,8 @@ static NSDictionary *ScriptableMethods() {
 }
 
 // Webview UI delegate method.
-- (NSArray *)webView:(WebView *)sender 
-    contextMenuItemsForElement:(NSDictionary *)element 
+- (NSArray *)webView:(WebView *)sender
+    contextMenuItemsForElement:(NSDictionary *)element
               defaultMenuItems:(NSArray *)defaultMenuItems {
   // disable contextual menus for dialogs.
   return nil;
@@ -245,7 +245,7 @@ static NSDictionary *ScriptableMethods() {
                       height: (int)height {
   NSString *frame_rect = NSStringFromRect(NSMakeRect(left, top, width, height));
   NSArray *args = [NSArray arrayWithObjects:url, frame_rect, nil];
-  
+
   // detachNewThreadSelector will retain self until after the thread exits.
   [NSThread detachNewThreadSelector:@selector(loadURL:)
                          toTarget:self
@@ -264,23 +264,23 @@ static NSDictionary *ScriptableMethods() {
 // and then calls back to displayImage: when done.
 - (void)loadURL:(NSArray *)arguments {
   NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-  
+
   assert([arguments count] == 2);
   NSString *url = [arguments objectAtIndex:0];
   NSRect frame = NSRectFromString([arguments objectAtIndex:1]);
-  
+
   // Make sure URL is escaped.
   url = [url stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-  
+
   // Download Icon Data.
   std::string16 url_utf16;
   [url string16:&url_utf16];
   std::string16 user_agent_utf16;
   [user_agent_ string16:&user_agent_utf16];
-  
+
   NSMutableData *icon_data = [[[NSMutableData alloc] init] autorelease];
   if (!GetURLDataAsNSData(url_utf16, user_agent_utf16, icon_data)) return;
-  
+
   NSArray *args = [NSArray arrayWithObjects:icon_data, NSStringFromRect(frame),
                                             nil];
   // args is retained until the selector returns.
@@ -295,22 +295,22 @@ static NSDictionary *ScriptableMethods() {
 - (void)displayImage:(NSArray *)arguments {
   // If the window was closed, don't do anything.
   if (window_dismissed_) return;
-  
+
   assert([arguments count] == 2);
   NSData *image_data = [arguments objectAtIndex:0];
   NSRect frame = NSRectFromString([arguments objectAtIndex:1]);
   NSView *content_view = [window_ contentView];
-  
+
   // Translate from DOM->NSView coordinates.
   NSRect bounds = [content_view bounds];
   frame.origin.y = bounds.size.height - frame.origin.y - frame.size.height;
-  
+
   NSImageView *iv = [[NSImageView alloc] initWithFrame:frame];
   NSImage *img = [[NSImage alloc] initWithData:image_data];
   [iv setImage:img];
   [img release];
   [content_view addSubview:iv];
-  
+
   // Anchor the image view to the top left of the image contents.
   // TODO(playmobil): reposition the image view on screen resize from JS
   // to get more consistent results.
@@ -327,18 +327,18 @@ static NSDictionary *ScriptableMethods() {
 static bool FocusPluginContainerWindow() {
   JsCallContext *call_context = BrowserUtils::GetCurrentJsCallContext();
   NPObject *window;
-  if (NPN_GetValue(call_context->js_context(), NPNVWindowNPObject, &window) != 
+  if (NPN_GetValue(call_context->js_context(), NPNVWindowNPObject, &window) !=
       NPERR_NO_ERROR) {
     return false;
   }
-  
+
   std::string script_utf8("window.focus()");
   NPString script = {script_utf8.data(), script_utf8.length()};
   ScopedNPVariant foo;
   if (!NPN_Evaluate(call_context->js_context(), window, &script, &foo)) {
     return false;
   }
-    
+
   return true;
 }
 
@@ -347,16 +347,16 @@ bool HtmlDialog::DoModalImpl(const char16 *html_filename, int width, int height,
   if (!FocusPluginContainerWindow()) {
     return false;
   }
-  
+
   // We've temporarily disalbed HTML dialogs till we can resolve the underlying
   // bug in WebKit.  For now we use Native dialogs.
-# if 0  
+# if 0
   HTMLDialogImp *dialog = [[[HTMLDialogImp alloc]
-                                 initWithHtmlFile:html_filename 
+                                 initWithHtmlFile:html_filename
                                         arguments:arguments_string
                                             width:width
                                            height:height] autorelease];
-  
+
   if (!dialog) {
     return false;
   }
@@ -370,7 +370,7 @@ bool HtmlDialog::DoModalImpl(const char16 *html_filename, int width, int height,
     return false;
   }
 #endif
-  
+
   SetResult(results.c_str());
   return true;
 }
@@ -378,9 +378,31 @@ bool HtmlDialog::DoModalImpl(const char16 *html_filename, int width, int height,
 bool HtmlDialog::GetLocale(std::string16 *locale) {
   assert(locale);
   NSLocale *current_locale = [NSLocale currentLocale];
-  NSString *language = [current_locale objectForKey:NSLocaleLanguageCode];
   NSString *country = [current_locale objectForKey:NSLocaleCountryCode];
-  
+
+  // The language reported by [NSLocale objectForKey:NSLocaleLanguageCode]
+  // isn't the real user interface language set in "International Preferences".
+  // For that we need to read the value from the user's defaults.
+  NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+  NSArray *languages = [defaults objectForKey:@"AppleLanguages"];
+  if ([languages count] == 0) {
+    return false;
+  }
+  NSString *raw_language = [languages objectAtIndex:0];
+
+  // According to http://developer.apple.com/qa/qa2006/qa1391.html
+  // we need to perform the following transformation to reliably get a
+  // canonical language code.
+  const NSString *language =
+      reinterpret_cast<const NSString *>(
+          CFLocaleCreateCanonicalLanguageIdentifierFromString(
+              kCFAllocatorDefault,
+              reinterpret_cast<CFStringRef>(raw_language)));
+
+  if (language == nil || country == nil) {
+    return false;
+  }
+
   // Put together a string like en-US.
   NSString *ret = [NSString stringWithFormat:@"%@-%@", language, country];
   [ret string16:locale];
