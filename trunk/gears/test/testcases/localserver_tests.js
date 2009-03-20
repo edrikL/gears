601f9db4b0edee274489c6a1f50dda888fcd8313
gears.daemon <gears.daemon@fe895e04-df30-0410-9975-d76d301b4276>
2009-03-20 21:00:50 +0000

    [Author: ace]
    Removing garbage character from js file that was causing syntax error in chrome/safari.
    
    TBR=michaeln
    OCL=10557465
    SCL=10557505

--- trunk/gears/test/testcases/localserver_tests.js
+++ trunk/gears/test/testcases/localserver_tests.js
@@ -1,4 +1,4 @@
-﻿// Copyright 2007, Google Inc.
+// Copyright 2007, Google Inc.
 //
 // Redistribution and use in source and binary forms, with or without 
 // modification, are permitted provided that the following conditions are met:
